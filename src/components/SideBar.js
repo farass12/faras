51c3de64a23c7ca1be020dbb9f43e77aa4c487b9
Farass12 <farasadiilahareta@gmail.com>
2025-08-20 13:57:40 +0700

    fix: add missing SideBar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <aside>
+      <h2>Sidebar</h2>
+      <p>Ini adalah sidebar default.</p>
+    </aside>
+  );
+};
+
+export default SideBar;
